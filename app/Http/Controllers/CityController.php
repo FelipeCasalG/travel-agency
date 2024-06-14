6d1b9d0f61f483f046bfbb1b4b8f0100c015daad
FelipeCasalG <felipecasalgaluppo@gmail.com>
2024-06-14 15:48:05 -0300

    feat: Add CityController with CRUD operations for cities
    This commit adds a new controller called CityController with CRUD (Create, Read, Update, Delete) operations for managing cities. It also includes the necessary routes in api.php and the setup of the api in the app.php file.

---
+++ app/Http/Controllers/CityController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\City;
+use Illuminate\Database\Eloquent\Casts\Json;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Validator;
+
+class CityController extends Controller
+{
+    public function index()
+    {
+        return view('cities.index');
+    }
+
+    public function all()
+    {
+        $cities = City::all();
+        return response()->json(['cities' => $cities]);
+    }
+
+    public function store(Request $request): JsonResponse
+    {
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'name' => 'required|string|max:255|unique:cities,name',
+            ]
+        );
+
+        if ($validator->fails()) {
+            return response()->json([
+                'error' => $validator->messages()
+            ], 400);
+        }
+
+        $city = City::create([
+            'name' => $request->input('name'),
+        ]);
+
+        return response()->json($city, 201);
+    }
+
+    public function update(Request $request, $id): JsonResponse
+    {
+        $validator = Validator::make($request->all(),
+            [
+                'name' => 'required|string|max:255|unique:cities,name',
+            ]
+        );
+
+        if ($validator->fails()) {
+            return response()->json([
+                'error' => $validator->messages()
+            ], 400);
+        }
+
+        $city = City::find($id);
+
+        if ($city) {
+            $city->name = $request->input('name');
+            $city->save();
+            return response()->json($city, 200);
+        } else {
+            return response()->json(['error' => 'City not found'], 404);
+        }
+    }
+
+    public function destroy($id): JsonResponse
+    {
+        $city = City::find($id);
+        if ($city) {
+            $city->delete();
+            return response()->json('City deleted successfully', 200);
+        } else {
+            return response()->json(['error' => 'City not found'], 404);
+        }
+    }
+    
+}
---
+++ routes/api.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CityController;
+
+Route::get('/cities', [CityController::class, 'all']);
+Route::post('/cities', [CityController::class, 'store']);
+Route::put('/cities/{id}', [CityController::class, 'update']);
+Route::delete('/cities/{id}', [CityController::class, 'destroy']);
