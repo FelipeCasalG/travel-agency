2e9ac298a9738e801f3a55451f2c875510a9cfd9
FelipeCasalG <felipecasalgaluppo@gmail.com>
2024-06-18 14:01:10 -0300

    feat: Handle invalid query parameters in GetAllCitiesController

--- app/Http/Controllers/GetAllCitiesController.php
+++ app/Http/Controllers/GetAllCitiesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\City;
 use App\Transformers\CityTransformer;
 use Flugg\Responder\Contracts\Responder;
+use Illuminate\Database\QueryException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
@@ -13,9 +14,12 @@ class GetAllCitiesController extends Controller
     public function execute(Request $request, Responder $responder): JsonResponse
     {
         $sort = $request->query('sort', 'id');
-        $order = $request->query('order', 'asc');
-        // sort and paginate by 8 the cities
-        $cities = City::orderBy($sort, $order)->paginate(8);
-        return $responder->success($cities, CityTransformer::class)->respond();
+        $order = $request->query('order', 'asc') === 'desc' ? 'desc' : 'asc';
+        try {
+            $cities = City::orderBy($sort, $order)->paginate(8);
+            return $responder->success($cities, new CityTransformer())->respond();
+        } catch (QueryException) {
+            return $responder->error(JsonResponse::HTTP_BAD_REQUEST, 'Invalid query parameters.')->respond();
+        }
     }
 }
