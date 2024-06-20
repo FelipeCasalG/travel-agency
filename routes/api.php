da9c3450d336a014eab0160d65dea1165708aca1
FelipeCasalG <felipecasalgaluppo@gmail.com>
2024-06-20 13:38:32 -0300

    fix: Update HTTP method for updating a city in API routes from patch to put

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,6 @@ use App\Http\Controllers\Cities\UpdateCityController;
 Route::prefix('cities')->name('cities.')->group(function () {
     Route::get('/', GetAllCitiesController::class)->name('index');
     Route::post('/', StoreCityController::class)->name('store');
-    Route::patch('/{id}', UpdateCityController::class)->name('update');
+    Route::put('/{id}', UpdateCityController::class)->name('update');
     Route::delete('/{id}', DestroyCityController::class)->name('destroy');
 });
